c094a3b5f31bc02a8174d442c08acd0286c4d5c1
Mariano Mariano <marianofheller@gmail.com>
2017-08-30 20:07:39 -0300

    Mejore radioGetter

--- src/radioGetter.js
+++ src/radioGetter.js
@@ -1,5 +1,6 @@
 var axios = require('axios');
 var fs = require('fs');
+var ProgressBar = require('progress');
 var addHours = require('date-fns/add_hours');
 var getTime = require('date-fns/get_time');
 var getDate = require('date-fns/get_date');
@@ -14,13 +15,19 @@ const radioChannel = "metro951";
 
 const getChunksInfo = function( timestamp, radioChannel ) {
 	const chunkServerUrl = `https://chunkserver.radiocut.fm/server/get_chunks`;
-	const targetUrl = `${chunkServerUrl}/${radioChannel}/${timestamp}/`;
+	const requestsArray = [];
+	timestamp.forEach( (t) => {
+		const targetUrl = `${chunkServerUrl}/${radioChannel}/${t}/`;
+		requestsArray.push( axios.get(targetUrl) );
+	});
 
-	console.log(`Getting chunks from: ${targetUrl}`);
-	return axios.get(targetUrl)
+	return axios.all(requestsArray)
 	.then( (response) => {
-		console.log(`Got ${response.data[timestamp].chunks.length} chunks`);
-		return response.data;
+		return response.map( (r) => {
+			const key = timestamp.map(String)
+						.find( (t) => Object.keys(r.data).includes(t) );
+			return Object.assign( r.data, { timestamp: key } );
+		});
 	} )
 	.catch( (e) => {
 		throw new Error(e);
@@ -30,9 +37,6 @@ const getChunksInfo = function( timestamp, radioChannel ) {
 const getChunk = function( filename, baseUrl, destDir ) {
 	const targetURL = `${baseUrl}/${filename}`;
 
-	console.log(`Getting audio file: ${filename}`);
-	console.log(`Target url: ${targetURL}`)
-
 	axios.get( targetURL )
 	.then( (response) => {
 
@@ -41,9 +45,21 @@ const getChunk = function( filename, baseUrl, destDir ) {
 
 		fs.writeFile( pathToFile, response.data, (err) => {
 			if (err) throw err;
-			console.log(`Saved file ${pathToFile} successfully`);
 		});
 	} )
+	.catch( (e) => {
+		throw new Error(e);
+	})
+}
+
+const getFiles = function( urls, destDir ) {
+	const requestsArray = urls.map( (url) => axios.get(url) );
+
+	axios.all( requestsArray )
+	.then( (responses) => {
+		if (!fs.existsSync(destDir)) fs.mkdirSync(destDir);
+		console.log(responses);
+	}) 
 	.catch( (e) => {
 		throw new Error(e);
 	})
@@ -63,7 +79,37 @@ const getRadioProgram = function( { radioChannel, startHour, endHour, emissionDa
 	const secStart = getTime(startDate)/1000;
 	const secEnd = getTime(endDate)/1000;
 
-	console.log(secStart, secEnd);
+	const chunksTimestamp = [];
+	for( let i = Math.floor(secStart/10000) ; i <= Math.floor(secEnd/10000) ; i++ ) {
+		chunksTimestamp.push(i);
+	}
+
+	getChunksInfo( chunksTimestamp, radioChannel )
+	.then( (datas) => {
+		//Filter chunks not in true time interval
+		return datas.map( (data) => {
+			const key = data.timestamp;
+			const newChunks = data[key].chunks.filter( (c) => c.start > secStart && c.start < secEnd );
+			data[key].chunks = newChunks;
+			return data;
+		});
+	})
+	.then( (datas) => {
+		//Parse file urls to array
+		return datas.reduce( (acc, data) => {
+			const key = data.timestamp;
+			const { baseURL } = data[key];
+			return acc.concat(
+				data[key].chunks.map( (c) => `${baseURL}/${c.filename}`)
+			);
+		}, [] );
+	})
+	.then( (urls) => {
+		getFiles(urls, tempDir);
+	})
+	.catch( (e) => {
+		console.log(e);
+	})
 }
 
 
@@ -74,15 +120,3 @@ getRadioProgram( {
 	emissionDate: new Date(),
 });
 
-
-/*
-getChunksInfo(targetDate, radioChannel)
-.then( (data) => {
-	const { baseURL } = data[targetDate];
-	getChunk( data[targetDate].chunks[0].filename, baseURL, tempDir);
-} )
-.catch( (e) => {
-	console.log(e);
-})
-
-*/
